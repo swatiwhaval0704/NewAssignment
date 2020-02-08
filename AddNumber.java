9417cd44b5703f11fd270ae5fb79b1c9ac05e2bf
swati.whaval <swatiwhaval@yahoo.co.in>
2020-02-08 07:22:06 +0000

    Firt Of Commit Of Program

---
+++ AddNumber.java
@@ -0,0 +1,12 @@
+
+public class AddTwoNumbers {
+
+   public static void main(String[] args) {
+//This is the program to add two integers        
+      int num1 = 5, num2 = 15, sum;
+      sum = num1 + num2;
+
+      System.out.println("Sum of these numbers: "+sum);
+   }
+}
+
